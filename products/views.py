be69922db31c2c4f3e591271bfe47c3308dc754c
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-05 13:23:20 +0000

    Add category filters to navigation, remove special offers menu entry

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product
+from .models import Product, SkillCategory, EnvCategory
 
 
 def all_products(request):
@@ -9,8 +9,20 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    skill_categories = None
+    env_categories = None
 
     if request.GET:
+        if 'skill-category' in request.GET:
+            skill_categories = request.GET['skill-category'].split(',')
+            products = products.filter(skill_category__name__in=skill_categories)
+            skill_categories = SkillCategory.objects.filter(name__in=skill_categories)
+
+        if 'env-category' in request.GET:
+            env_categories = request.GET['env-category'].split(',')
+            products = products.filter(env_category__name__in=env_categories)
+            env_categories = EnvCategory.objects.filter(name__in=env_categories)
+
         if 'searchq' in request.GET:
             query = request.GET['searchq']
             if not query:
@@ -21,7 +33,10 @@ def all_products(request):
             products = products.filter(queries)
 
     context = {'products': products,
-               'search_term': query,}
+               'search_term': query,
+               'curr_skill_categories': skill_categories,
+               'curr_env_categories': env_categories, }
+
     return render(request, 'products/products.html', context)
 
 
