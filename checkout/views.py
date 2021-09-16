b84f9baa42380f12fcb8130050fabc1726455007
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-16 14:13:21 +0000

    Add stripe js, stripe card element to checkout page, style with css to align with the page

--- checkout/views.py
+++ checkout/views.py
@@ -11,5 +11,9 @@ def checkout(request):
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
-    context = { 'order_form': order_form, }
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JTU0bLStlNmediIiws2Rl2Vu4giKbZ5j2tGI8BM6BOJsPZNxYLlho7KZNEy4RQHKYJBwLSkL3EaiqCX5UA9ffvu00YQJvGFMs',
+        'client_secret': 'sk_test_51JTU0bLStlNmediInoGBuWUJ2F3WVzDyrSEcCs02ERXry5wc0eQpd9l1lUZ6E07z2FRMj5IcuxsIzlT6cMYTjPm300lbCl8nPP',
+        }
     return render(request, template, context)
