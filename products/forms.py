04606f430aeeef43b41c5d2b0c5485f9ab49201c
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-19 10:49:01 +0000

    Add product form template for superusers

---
+++ products/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Product, EnvCategory, SkillCategory
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        skill_categories = SkillCategory.objects.all()
+        skill_friendly_names = [(c.id, c.get_friendly_name()) for c in skill_categories]
+
+        env_categories = EnvCategory.objects.all()
+        env_friendly_names = [(c.id, c.get_friendly_name()) for c in env_categories]
+
+        self.fields['skill_category'].choices = skill_friendly_names
+        self.fields['env_category'].choices = env_friendly_names
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
