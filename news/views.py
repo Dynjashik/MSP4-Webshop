8c101be6a135b785e132a5701ab2c46a70d9f505
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-22 16:05:16 +0000

    Add first draft of news page with css and views links

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from django.shortcuts import render, redirect, reverse, get_object_or_404
@@ -9,6 +10,34 @@ def all_news(request):
     """A view to show all news"""
 
     all_news = News.objects.all()
-    context = {'news_in_html': all_news}
+    context = {'news': all_news}
 
     return render(request, 'news/news.html', context)
+
+
+@login_required
+def add_news_item(request):
+    """ Add news article """
+
+    return redirect(reverse('news'))
+
+
+@login_required
+def edit_news_item(request, news_id):
+    """ Edit news item """
+
+    return redirect(reverse('news'))
+
+
+@login_required
+def delete_news_item(request, news_id):
+    """ Delete news item """
+
+    #if not request.user.is_superuser:
+    #    messages.error(request, 'Sorry, only store owners can do that.')
+    #    return redirect(reverse('home'))
+
+    #product = get_object_or_404(Product, pk=product_id)
+    #product.delete()
+    #messages.success(request, 'Product deleted!')
+    return redirect(reverse('news'))
