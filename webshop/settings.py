1f40433e26befe149cfecf4f866b82b543ef3177
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-25 19:24:40 +0000

    Update SECRET_KEY and DEBUG values in settings.py

--- webshop/settings.py
+++ webshop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3m(j=wiah&gl&u-tp3yj9&)w&a*0%t8-7txgon-9c@hem(c&i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ["baby-hero.herokuapp.com", "localhost"]
 
