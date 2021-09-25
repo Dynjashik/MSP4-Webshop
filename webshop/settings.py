e3ed5849c84d35d10b07269599cd6efc15bee285
Dynjashik <stasenko.dasha.s@gmail.com>
2021-09-25 18:47:26 +0000

    Add Procfile, update requirements.txt and modify settings.py for deploying to Heroku

--- webshop/settings.py
+++ webshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-_3m(j=wiah&gl&u-tp3yj9&)w&a*0%t8-7txgon-9c@hem(c&i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["baby-hero.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'news',
+    'contact',
 
     # External
     'crispy_forms',
@@ -118,12 +120,16 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 
 # Password validation
