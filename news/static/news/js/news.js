9dc338bc12628ccebfa856f78ab4ffd2c18984f3
Dynjashik <stasenko.dasha.s@gmail.com>
2021-10-11 20:05:15 +0000

    Fixed footer padding for all long pages

--- news/static/news/js/news.js
+++ news/static/news/js/news.js
@@ -2,3 +2,8 @@ $('#new-image').change(function () {
     var file = $('#new-image')[0].files[0];
     $('#filename').text(`Image will be set to: ${file.name}`);
 });
+
+/** Button to go to top of the page */
+$('.btt-link').click(function (e) {
+    window.scrollTo(0, 0);
+});
